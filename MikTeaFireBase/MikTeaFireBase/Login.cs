f0931041e1e0e1abff436a58880d0bc55a9b5e4c
Nguyễn Thị Tiên Ngân <thienhatu1998@gmail.com>
2019-12-17 12:52:37 +0700

    login

---
+++ MikTeaFireBase/MikTeaFireBase/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace MikTeaFireBase
+{
+    public partial class Login : DevExpress.XtraEditors.XtraForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
